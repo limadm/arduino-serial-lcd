48e734e1dabf4618a7bf56e272f2603d01951eaf
Daniel Lima <danielm@nanohub.tk>
2017-09-04 13:49:59 -0300

    compilation errors + indentation

--- SerialLCD.h
+++ SerialLCD.h
@@ -18,24 +18,24 @@
 #define LCD_SETDDRAMADDR 0x80
 
 // flags for display entry mode
+#define LCD_ENTRYLEFT  0x02
 #define LCD_ENTRYRIGHT 0x00
-#define LCD_ENTRYLEFT 0x02
 #define LCD_ENTRYSHIFTINCREMENT 0x01
 #define LCD_ENTRYSHIFTDECREMENT 0x00
 
 // flags for display on/off control
-#define LCD_DISPLAYON 0x04
+#define LCD_DISPLAYON  0x04
 #define LCD_DISPLAYOFF 0x00
-#define LCD_CURSORON 0x02
+#define LCD_CURSORON  0x02
 #define LCD_CURSOROFF 0x00
-#define LCD_BLINKON 0x01
+#define LCD_BLINKON  0x01
 #define LCD_BLINKOFF 0x00
 
 // flags for display/cursor shift
 #define LCD_DISPLAYMOVE 0x08
-#define LCD_CURSORMOVE 0x00
+#define LCD_CURSORMOVE  0x00
 #define LCD_MOVERIGHT 0x04
-#define LCD_MOVELEFT 0x00
+#define LCD_MOVELEFT  0x00
 
 // flags for function set
 #define LCD_8BITMODE 0x10
@@ -43,22 +43,24 @@
 #define LCD_2LINE 0x08
 #define LCD_1LINE 0x00
 #define LCD_5x10DOTS 0x04
-#define LCD_5x8DOTS 0x00
+#define LCD_5x8DOTS  0x00
 
-template <int RS, int EN, int D4, int D5, int D6, int D7>
 class SerialLCD : public Print {
 	public:
-		SerialLCD(ShiftRegister &io) :
+		SerialLCD(ShiftRegister &io,
+				uint8_t RS, uint8_t EN, uint8_t D4,
+				uint8_t D5, uint8_t D6, uint8_t D7)
+			: RS(RS), EN(EN), D4(D4), D5(D5), D6(D6), D7(D7),
 			_io(io), _lines(), _offsets(), _func(), _mode(),
 			_ctrl(LCD_DISPLAYON | LCD_CURSOROFF | LCD_BLINKOFF)
-		{
-			begin(16, 2);
-		}
+	{
+		begin(16, 2);
+	}
 		void begin(uint8_t cols, uint8_t rows, uint8_t charsize = LCD_5x8DOTS) {
 			_lines = rows;
 			if (_lines > 1) _func |= LCD_2LINE;
 			setRowOffsets(0, 0x40, cols, 0x40+cols);
-			if (charsize != LCD_5x8DOTS && lines==1) _func = LCD_5x10DOTS;
+			if (charsize != LCD_5x8DOTS && _lines==1) _func = LCD_5x10DOTS;
 			// LCD 4-bit mode power-up sequence (datasheet p.45-46)
 			delayMicroseconds(50000);
 			_io.write(RS, LOW);
@@ -92,13 +94,14 @@ class SerialLCD : public Print {
 
 		void scrollDisplayLeft()  { command(LCD_CURSORSHIFT | LCD_DISPLAYMOVE | LCD_MOVELEFT ); }
 		void scrollDisplayRight() { command(LCD_CURSORSHIFT | LCD_DISPLAYMOVE | LCD_MOVERIGHT); }
-		void leftToRight()        { command(LCD_ENTRYMODESET | (_mode |=  LCD_ENTRYLEFT); }
-		void rightToLeft()        { command(LCD_ENTRYMODESET | (_mode &= ~LCD_ENTRYLEFT); }
-		void autoscroll()         { command(LCD_ENTRYMODESET | (_mode |=  LCD_ENTRYSHIFTINCREMENT); }
-		void noAutoscroll()       { command(LCD_ENTRYMODESET | (_mode &= ~LCD_ENTRYSHIFTINCREMENT); }
+		void leftToRight()        { command(LCD_ENTRYMODESET | (_mode |=  LCD_ENTRYLEFT)); }
+		void rightToLeft()        { command(LCD_ENTRYMODESET | (_mode &= ~LCD_ENTRYLEFT)); }
+		void autoscroll()         { command(LCD_ENTRYMODESET | (_mode |=  LCD_ENTRYSHIFTINCREMENT)); }
+		void noAutoscroll()       { command(LCD_ENTRYMODESET | (_mode &= ~LCD_ENTRYSHIFTINCREMENT)); }
 
 		void setRowOffsets(int a, int b, int c, int d) {
-			_offsets = {a,b,c,d};
+			_offsets[0] = a; _offsets[1] = b;
+			_offsets[2] = c; _offsets[3] = d;
 		}
 		void setCursor(uint8_t col, uint8_t row) {
 			command(LCD_SETDDRAMADDR | (col + _offsets[row%_lines]));
@@ -131,9 +134,7 @@ class SerialLCD : public Print {
 		}
 
 		ShiftRegister &_io;
-		uint8_t _func;
-		uint8_t _ctrl;
-		uint8_t _mode;
-		uint8_t _lines;
+		const uint8_t RS,EN,D4,D5,D6,D7;
+		uint8_t _func, _ctrl, _mode, _lines;
 		uint8_t _offsets[4];
 };
